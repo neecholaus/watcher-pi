af0852b365754fbf9e3695d54dfbc81baee15539
neecholaus <thisisnickadam@gmail.com>
2019-07-14 17:59:55 -0700

    Only uploads captures with enough of a difference.

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ require('dotenv').config();
 
 const fs = require('fs');
 const axios = require('axios');
-const {exec} = require('child_process');
+const {execSync} = require('child_process');
 const hlp = require('./helpers.js');
+const pixelmatch = require('pixelmatch');
+const PNG = require('pngjs').PNG;
+const JPG = require('jpeg-js');
 
 const {IMAGE_DIR, API_USER, API_PASS} = process.env;
 
 function watch() {
-    console.log('Taking image...');
+    console.log('taking image...');
     const filename = Date.now().toString();
-    let cmd = `raspistill -o ${IMAGE_DIR}/${filename}.jpg`;
-    exec(cmd);
+    let cmd = `raspistill -o ${IMAGE_DIR}/${filename}.jpg -n --timeout 1`;
+    execSync(cmd);
 
     fs.readdir(IMAGE_DIR, (err, data) => {
         if(err) return hlp.error(err);
         if(data.length < 1) {
-            console.log('No images found.');
-            setTimeout(watch,1000);
+            console.log('no images found');
+	    watch();
+            //setTimeout(watch,1000);
             return;
         }
 
@@ -31,19 +35,42 @@ function watch() {
 
         // Make sure there is still an image after stripping non png/jpg files
         if(!fs.existsSync(`${IMAGE_DIR}/${newest}`)) {
-            if(data.length > 0) console.log('No images found.');
-            else console.log('Image was not found.');
-            setTimeout(watch,1000);
+            if(data.length > 0) console.log('no images found');
+            else console.log('image was not found');
+	    watch();
+            //setTimeout(watch,1000);
             return;
         }
 
         // Returns file as a buffer
+	console.log('comparing images');
         const file = fs.readFileSync(`${IMAGE_DIR}/${newest}`);
+
+	let currJpg = JPG.decode(file);
+	let oldJpg = JPG.decode(fs.readFileSync(`${IMAGE_DIR}/old.jpg`));
+	
+	let pxDiff = pixelmatch(
+		currJpg.data,
+		oldJpg.data,
+		null,
+		currJpg.width,
+		currJpg.height,
+		{threshold: 0.3}
+	);
+
+	fs.writeFileSync(`${IMAGE_DIR}/old.jpg`, file);
         fs.unlinkSync(`${IMAGE_DIR}/${newest}`);
 
         const API_LOCAL_URL = 'http://localhost:3000/watcher/api-upload'
         const API_LIVE_URL = 'https://nickneuman.co/watcher/api-upload'
 
+	if(pxDiff) {
+	    console.log('no notable changes, skipping upload');
+	    watch();
+	    return;
+	}
+
+	console.log('attempting upload...');
         axios({
             url: API_LIVE_URL,
             method: 'POST',
@@ -54,11 +81,15 @@ function watch() {
             }
         })
             .then(res => {
-                console.log('Image has been uploaded.');
+                console.log('upload attempt was successful');
+		watch();
             })
-            .catch(err => console.log('Server Error'));
+            .catch(err => {
+	        console.log('server encountered an error');
+		watch();
+	    });
 
-        setTimeout(watch,1000);
+        //setTimeout(watch,1000);
     });
 }
 
